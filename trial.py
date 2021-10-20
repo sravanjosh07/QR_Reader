2571c35c8fdb7412fecca168274eec109ae4bc6f
Sravan Josh <sravanjoshh@gmail.com>
2021-10-20 17:22:42 +0200

    using imutils

--- trial.py
+++ trial.py
@@ -356,8 +356,43 @@
 # def read_vedio(cap):
 #     pass
 #
-#
-#
-#
-#
-#
+
+
+from imutils.video import VideoStream
+from pyzbar import pyzbar
+import argparse
+import datetime
+import imutils
+import time
+import cv2
+
+ap = argparse.ArgumentParser()
+ap.add_argument("-o", "--output", type = str, default="barcodes.csv", help = "path to output CSF file containing barcodes")
+args = vars(ap.parse_args())
+cap = VideoStream(src=1).start()
+time.sleep(2.0)
+csv = open(args["output"], "w")
+found = set()
+
+while True:
+    frame = cap.read()
+    frame = cv2.bitwise_not(frame)
+    frame = frame[:200, :200]
+    barcodes = pyzbar.decode(frame)
+    for barcode in barcodes:
+        barcodeData = barcode.data.decode("utf-8")
+        if barcodeData not in found:
+            csv.write("{},{}\n".format(datetime.datetime.now(), barcodeData))
+            csv.flush()
+            found.add(barcodeData)
+
+    cv2.imshow("BarcodeScanner", frame)
+    key = cv2.waitKey(1)& 0xFF
+    if key == ord("q"):
+        break
+
+
+print("[INFO] cleaning up...")
+csv.close()
+cv2.destroyAllWindows()
+vs.stop()
