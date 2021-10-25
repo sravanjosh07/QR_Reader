d9bbd17458df8dbc5e7043287fe3c8a52b6d15f8
Sravan Josh <sravanjoshh@gmail.com>
2021-10-25 09:34:38 +0200

    second commit

--- main.py
+++ main.py
@@ -3,33 +3,52 @@ from pyzbar.pyzbar import decode
 import time
 import winsound
 import datetime
-import csv
 
-cap = cv2.VideoCapture(1)
-scanned_codes = []
+cap = cv2.VideoCapture(0)
+scanned_code_list = []
+scanned_data_dict: dict[str, datetime.datetime] = {}  # key: code, value: last detection
 csv = open("scanned_QRcodes.csv", "w")
 camera = True
-
+square = 200
 while camera == True:
     ret, frame = cap.read()
-    frame = cv2.bitwise_not(frame)
-    frame = frame[:200, :200]
 
-    for code in decode(frame):
+    frame_crop = frame[-200:, int(frame.shape[1]/2-square/2):int(frame.shape[1]/2+square/2)]
+    frame_crop = cv2.bitwise_not(frame_crop)
+    frame_crop = cv2.cvtColor(frame_crop, cv2.COLOR_BGR2GRAY)
+    ret, frame_crop = cv2.threshold(frame_crop, 0, 255, cv2.THRESH_OTSU)
+
+    # cv2.imshow("QR_Scanner thresh", frame_crop)
+
+    now = datetime.datetime.now()
+    x = int(time.time())
+
+    for code in decode(frame_crop):
         mycode = code.data.decode("utf-8")
-        if mycode not in scanned_codes:
-            print(mycode)
-            scanned_codes.append(mycode)
-            csv.write("{},{}\n".format(datetime.datetime.now(), mycode))
-            csv.flush()
-            winsound.Beep( 500, 500)
-            time.sleep(5)
-        elif mycode in scanned_codes:
-            print(mycode)
-            winsound.Beep(1000, 1000)
-            time.sleep(5)
-        else:
-            pass
 
+
+        if mycode not in scanned_code_list:
+            print(mycode)
+            scanned_code_list.append(mycode)
+            csv.write("{},{}\n".format(datetime.datetime.now(), mycode))
+            scanned_data_dict["mycode"] = x
+            csv.flush()
+            # winsound.Beep(500, 500)
+
+        elif mycode in scanned_code_list and x - scanned_data_dict["mycode"] > 5:
+            # scanned_data_dict.update({"mycode": x}) #clearing the list will rewrite the dict as it's going to get scanned again
+            scanned_code_list.clear()
+            break
+
+        else:
+            continue
+
+    frame = cv2.rectangle(
+        frame,
+        (int(frame.shape[1]/2-square/2), frame.shape[0]-square),
+        (int(frame.shape[1]/2+square/2), frame.shape[0]),
+        (0, 255, 0),
+        thickness=3)
     cv2.imshow("QR_Scanner", frame)
     cv2.waitKey(1)
+print(scanned_data_dict)
