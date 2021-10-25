9ec8fca635be1add816e8e97ee6f9d44ff05968a
Sravan Josh <sravanjoshh@gmail.com>
2021-10-25 16:56:22 +0200

    fourth commit

--- main.py
+++ main.py
@@ -7,20 +7,30 @@ import datetime
 cap = cv2.VideoCapture(0)
 scanned_code_list = []
 scanned_data_dict: dict[str, datetime.datetime] = {}  # key: code, value: last detection
-csv = open("scanned_QRcodes.csv", "w")
+# csv = open("scanned_QRcodes.csv", "w")
 camera = True
 square = 200
+scale_percent = 70  # percent of original size
+
+
+
 while camera == True:
     ret, frame = cap.read()
 
-    frame_crop = frame[-200:, int(frame.shape[1]/2-square/2):int(frame.shape[1]/2+square/2)]
+    '''
+    scaling the frame, because opencv cant show large images in a single window'''
+    width = int(frame.shape[1] * scale_percent / 100)
+    height = int(frame.shape[0] * scale_percent / 100)
+    dim = (width, height)
+    # resize image
+
+    frame = cv2.resize(frame, dim, interpolation=cv2.INTER_AREA)
+
+    frame_crop = frame[-square:, int(frame.shape[1]/2-square/2):int(frame.shape[1]/2+square/2)]
     frame_crop = cv2.bitwise_not(frame_crop)
     frame_crop = cv2.cvtColor(frame_crop, cv2.COLOR_BGR2GRAY)
     ret, frame_crop = cv2.threshold(frame_crop, 0, 255, cv2.THRESH_OTSU)
 
-    # cv2.imshow("QR_Scanner thresh", frame_crop)
-
-    now = datetime.datetime.now()
     x = int(time.time())
 
     for code in decode(frame_crop):
@@ -30,15 +40,15 @@ while camera == True:
         if mycode not in scanned_code_list:
             print(mycode)
             scanned_code_list.append(mycode)
-            csv.write("{},{}\n".format(datetime.datetime.now(), mycode))
+            # csv.write("{},{}\n".format(datetime.datetime.now(), mycode))
             scanned_data_dict["mycode"] = x
-            csv.flush()
-            # winsound.Beep(500, 500)
+            # csv.flush()
+            winsound.Beep(500, 500)
 
         elif mycode in scanned_code_list and x - scanned_data_dict["mycode"] > 5:
             # scanned_data_dict.update({"mycode": x}) #clearing the list will rewrite the dict as it's going to get scanned again
             scanned_code_list.clear()
-            break
+            continue
 
         else:
             continue
@@ -50,5 +60,6 @@ while camera == True:
         (0, 255, 0),
         thickness=3)
     cv2.imshow("QR_Scanner", frame)
+    # cv2.imshow("QR_Scanner thresh", frame_crop)
     cv2.waitKey(1)
 print(scanned_data_dict)
