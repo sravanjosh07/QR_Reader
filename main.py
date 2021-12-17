5da37644da36b1bade8682ec2e92bfd0b0b00211
Sravan Josh <45886202+sravanjosh07@users.noreply.github.com>
2021-12-17 13:05:00 +0100

    added video files for evaluation

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import time
 import winsound
 import datetime
 
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture('img_data/slow1_with_light.h264')
 scanned_code_list = []
 scanned_data_dict: dict[str, datetime.datetime] = {}  # key: code, value: last detection
 # csv = open("scanned_QRcodes.csv", "w")
@@ -12,29 +12,21 @@ camera = True
 square = 200
 scale_percent = 70  # percent of original size
 
-
-
 while camera == True:
     ret, frame = cap.read()
-    '''
-    scaling the frame, because opencv cant show large images in a single window'''
-    width = int(frame.shape[1] * scale_percent / 100)
-    height = int(frame.shape[0] * scale_percent / 100)
-    dim = (width, height)
-    # resize image
+    frame = cv2.bitwise_not(frame)
+    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    # frame = cv2.blur(frame, (3,3))
+    # ret, thresh1 = cv2.threshold(frame, 0, 255, cv2.THRESH_TOZERO)
 
-    frame = cv2.resize(frame, dim, interpolation=cv2.INTER_AREA)
-    frame_crop = frame[-square:, int(frame.shape[1]/2-square/2):int(frame.shape[1]/2+square/2)]
-    frame_crop = cv2.bitwise_not(frame_crop)
-    frame_crop = cv2.cvtColor(frame_crop, cv2.COLOR_BGR2GRAY)
-    # frame_crop = cv2.blur(frame_crop, (3,3))
-    ret, frame_crop = cv2.threshold(frame_crop, 0, 255, cv2.THRESH_TOZERO)
+    # thresh1 = cv2.adaptiveThreshold(frame, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 97, 43)
+    thresh1 = cv2.adaptiveThreshold(frame, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, \
+                                cv2.THRESH_BINARY, 11, 7)
 
     x = int(time.time())
 
-    for code in decode(frame_crop):
+    for code in decode(thresh1):
         mycode = code.data.decode("utf-8")
-
 
         if mycode not in scanned_code_list:
             print(mycode)
@@ -42,23 +34,17 @@ while camera == True:
             # csv.write("{},{}\n".format(datetime.datetime.now(), mycode))
             scanned_data_dict["mycode"] = x
             # csv.flush()
-            winsound.Beep(500, 500)
 
         elif mycode in scanned_code_list and x - scanned_data_dict["mycode"] > 5:
-            # scanned_data_dict.update({"mycode": x}) #clearing the list will rewrite the dict as it's going to get scanned again
+            scanned_data_dict.update(
+                {"mycode": x})  # clearing the list will rewrite the dict as it's going to get scanned again
             scanned_code_list.clear()
             continue
 
         else:
             continue
 
-    frame = cv2.rectangle(
-        frame,
-        (int(frame.shape[1]/2-square/2), frame.shape[0]-square),
-        (int(frame.shape[1]/2+square/2), frame.shape[0]),
-        (0, 255, 0),
-        thickness=3)
     cv2.imshow("QR_Scanner", frame)
-    cv2.imshow("QR_Scanner thresh", frame_crop)
+    cv2.imshow("QR_Scanner thresh", thresh1)
     cv2.waitKey(1)
 print(scanned_data_dict)
